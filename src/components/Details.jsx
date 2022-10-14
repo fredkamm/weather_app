7d521626d909994df2671fdaea15e4158434bf13
fredkamm <fred.kamm95@gmail.com>
2022-10-14 12:26:48 -0400

    details section updated

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import {
+  UilTemperature,
+  UilTear,
+  UilWind,
+  UilSun,
+  UilSunset,
+} from "@iconscout/react-unicons";
 
 function Details() {
   return (
-    <div>Details</div>
-  )
+    <div>
+      <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+        <p>Cloudy</p>
+      </div>
+
+      <div className="flex flex-row items-center justify-between text-white py-3">
+        <img
+          src="http://openweathermap.org/img/wn/01d@2x.png"
+          alt="sunny"
+          className="w-20"
+        />
+        <p className="text-5xl">30</p>
+        <div className="flex flex-col space-y-2">
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTemperature size={18} className="mr-1" />
+            Feels like:
+            <span className="font-medium ml-1">32</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTear size={18} className="mr-1" />
+            Humidity
+            <span className="font-medium ml-1">32%</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilWind size={18} className="mr-1" />
+            Wind:
+            <span className="font-medium ml-1">11 MPH</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+        <UilSun />
+        <p className="font-light">
+          Rise:
+          <span className="font-medium ml-1">6:00 AM</span>
+        </p>
+        <p className="font-light">|</p>
+        <UilSunset />
+        <p className="font-light">
+          Rise:
+          <span className="font-medium ml-1">6:00 PM</span>
+        </p>
+        <p className="font-light">|</p>
+        <UilSun />
+        <p className="font-light">
+          High:
+          <span className="font-medium ml-1">65</span>
+        </p>
+        <UilSun />
+        <p className="font-light">
+          Low:
+          <span className="font-medium ml-1">32</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Details
+export default Details;
