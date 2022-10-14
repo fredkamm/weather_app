90ef969842383906d17e9eaf2e4a1129d74a4f4c
fredkamm <fred.kamm95@gmail.com>
2022-10-14 14:16:46 -0400

    weather api connected

--- src/Utils/WeatherApi.js
+++ src/Utils/WeatherApi.js
@@ -1,9 +1,9 @@
-const API_KEY = "16915ed41d608af251c5d1ca19c246be";
+const KEY = "16915ed41d608af251c5d1ca19c246be";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const weatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
-  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+  url.search = new URLSearchParams({ ...searchParams, appid: KEY });
 
   return fetch(url)
     .then((res) => res.json())
