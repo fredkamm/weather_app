666b70c44c95aae306ac355b32fd41fe2553f19c
fredkamm <fred.kamm95@gmail.com>
2022-10-14 10:23:15 -0400

    unicon install

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <UilReact />
     </div>
   );
 }
