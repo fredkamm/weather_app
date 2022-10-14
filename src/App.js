7802e80a120428fd87882de39a3559a1d61fa5da
fredkamm <fred.kamm95@gmail.com>
2022-10-14 12:04:42 -0400

    css update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import TopButton from './components/TopButton'
 import Inputs from './components/Inputs'
-// import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TimeLocation from './components/TimeLocation'
+import Details from './components/Details'
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
       <TopButton />
       <Inputs />
+      <TimeLocation />
+      <Details />
+      
     </div>
   );
 }
---
+++ src/components/TimeLocation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TimeLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">Current Time</p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Current location</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeLocation;
